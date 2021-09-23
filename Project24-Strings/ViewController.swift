4dbbc0724dc94f1c9d39df4bac98c1020602eec8
GilCND <fgilcnd@gmail.com>
2021-09-23 16:41:53 -0300

    [Project24-Strings-01] Added Strings extensions

--- Project24-Strings/ViewController.swift
+++ Project24-Strings/ViewController.swift
@@ -11,9 +11,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var number = ["aaaa", "1234", "asd"]
+        var test = "tes"
+        var test2 = "testa"
+        for element in number {
+            print (element.hasNumber())
+        }
+        number[0].addPrefix(prefix: "test")
+        print (number[0])
+        test.addPrefix(prefix: "test")
+        print (test)
+        test2.addPrefix(prefix: "test")
+        print (test2)
     }
 
 
-}
 
+}
+extension String {
+    
+    mutating func addPrefix(prefix: String) {
+        if self.contains(prefix){
+            print("It contains the prefix")
+        }
+        else{
+            self = prefix + self
+        }
+    }
+
+    func hasNumber() -> Bool {
+        var isNumber: Bool {
+            return !isEmpty && rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil
+        }
+        return isNumber
+    }
+   
+}
